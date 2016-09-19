9b687f5a45b734a1ca36285023c9830119363bff
George <skywalqqer@yandex.ru>
2016-09-19 22:39:22 +0300

    add auth from scratch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,21 @@
 class UsersController < ApplicationController
   def new
-    user = User.new
+    @user = User.new
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_url, notice: 'Thank you for signing in!'
     else
-      render new
+      render :new
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+  resource :users
+  resource :sessions
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

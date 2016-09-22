eb12223c8f0d8771aa5e230a747c265ba13e2356
George <skywalqqer@yandex.ru>
2016-09-22 23:08:27 +0300

    refactoring some file

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe PostsController, type: :controller do
   describe 'GET #index' do
     before(:each) { get :index }
+
     it { is_expected.to render_template(:index) }
     it { is_expected.to respond_with(200) }
+
     it 'populates an array of posts' do
       posts = create(:user_with_posts).posts
       expect(assigns(:posts)).to eq(posts)
@@ -13,6 +15,7 @@ RSpec.describe PostsController, type: :controller do
 
   describe 'GET #new' do
     before(:each) { get :new }
+    
     it { is_expected.to render_template(:new) }
     it { is_expected.to respond_with(200) }
   end

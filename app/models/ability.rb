d47a5a58fb2bc5df611fe5bc579acb30f593dbd6
George <skywalqqer@yandex.ru>
2016-09-28 21:10:14 +0300

    update abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,6 +6,7 @@ class Ability
     #
     user ||= User.new # guest user (not logged in)
     can :manage, :all if user.role == 'admin'
+    can :edit, Post, user_id: user.id
     #   if user.admin?
     #     can :manage, :all
     #   else

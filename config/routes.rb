6c7191b18139729c409b6d8341d43911bf643795
George <skywalqqer@yandex.ru>
2016-10-03 00:12:18 +0300

    add manage posts

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get '', to: 'dashboard#index'
+    resources :posts
   end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -19,8 +19,28 @@ feature 'accesses the dashborad' do
   scenario 'its denied access when not admin or not logged' do
     sign_in(user, '123')
     expect(page).to have_content('Welcome')
-    
+
     visit admin_path
     expect(page).to have_content('Access Denied!')
+  end
+end
+
+feature 'article managment' do
+  given!(:admin) { create(:user, password: '123', password_confirmation: '123', role: 'admin') }
+  before(:each) { sign_in(admin, '123') }
+  
+  scenario 'adds article' do
+    click_link 'Manage Articles'
+
+    expect(current_path).to eq(admin_posts_path)
+    expect{
+      click_link 'New Article'
+      fill_in 'Title', with: 'It is a new post'
+      fill_in 'Text', with: 'Some text body'
+      click_button 'Create Article'
+    }.to change(Post, :count).by(1)
+
+    expect(current_path).to eq(admin_posts_path)
+    expect(page). to have_content('It is a new post')
   end
 end

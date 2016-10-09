63c289f374d17fabd14c4b55c9ba0103ac7660d8
George <skywalqqer@yandex.ru>
2016-10-09 21:52:35 +0300

    update auth from twitter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,17 +5,17 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
+    if request.env['omniauth.auth']
+      user = User.from_omniauth(request.env['omniauth.auth'])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Welcome!'
+    elsif user && user.authenticate(params[:password])
       session[:user_id] = user.id
       if user.role == 'admin'
         redirect_to admin_path, notice: 'Logged in!'
       else
         redirect_to root_path, notice: 'Welcome!'
       end
-    elsif request.env['omniauth.auth']
-      user = User.from_omniauth(request.env['omniauth.auth'])
-      session[:user_id] = user.id
-      redirect_to root_path, notice: 'Welcome!'
     else
       flash.now.alert = 'Email or password is invalid'
       render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     def from_omniauth(auth_hash)
       user = find_or_create_by(uid: auth_hash[:uid], provider: auth_hash[:provider])
       user.name = auth_hash[:info][:name]
-      user.save!
+      user.save!(validate: false)
       user
     end
   end
